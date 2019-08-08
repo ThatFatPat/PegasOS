ea6396d3d6026e2121e3795028495d260db22f5a
ThatFatPat <idoshav@gmail.com>
2019-08-08 18:43:09 +0300

    Add GDT data, Add comments, start work on switching to 32bit

--- boot.asm
+++ boot.asm
@@ -12,23 +12,23 @@ print_drive:
     call print_string
     mov si, sp
     mov dx, [si]
-    call print_hex
+    call print_hex ; Print Drive Number, originates in DL and obtained through stack
     call print_newline
 
 reset_disk:
-    xor ah, ah
+    xor ah, ah ; INT13h, AH=00h : Reset Disk Services
     int 0x13
 
-; read_sectors:
+read_sectors:
     mov cx, 0x2 ; Cylinder 0, sector 2
     xor dx, dx  ; Track 0
-    mov si, sp
+    mov si, sp ; Cannot address using SP.
     mov dl, [si] ; Get DL = disk number
-    mov ah, 0x2
+    mov ah, 0x2 ; Sub command INT13h AH=02h : Read Sectors from disk
     mov al, 0x1 ; Number of sectors to read
     xor bx, bx
-    mov es, bx
-    mov bx, 0x8001
+    mov es, bx ; Zero out ES
+    mov bx, 0x8001 ; Starting address
     int 0x13
 
 print_number_of_read_sectors:
@@ -36,15 +36,17 @@ print_number_of_read_sectors:
     mov dx, 0x5
     call print_string
     xor bx, bx
-    call print_number
+    call print_number ; Print number of sectors read, saved in AL.
     lea bx, [read_sectors_string + 0x5]
     mov dx, 0x8
-    call print_string
+    call print_string ; Print rest of read_sectors_string.
 
 
 cleanup:
     add sp, 2
 
+load_gdt:
+    cli ; Disable interrupts
 
 fallback:
     hlt
@@ -56,6 +58,50 @@ booted_from_drive_string:
     db "Booted from drive "
 read_sectors_string:
     db "Read ", " sectors"
+
+; --- THIS PART OF THE CODE IS COPIED FROM OS-DEV.pdf, I READ THROUGH THE EXPLANATION AND IT'S POINTLESS TO CRAFT THIS MYSELF AS OF NOW. ---
+; GDT
+gdt_start :
+gdt_null : ; the mandatory null descriptor
+dd 0x0 ; ’dd ’ means define double word ( i.e. 4 bytes )
+dd 0x0
+gdt_code : ; the code segment descriptor
+; base =0x0 , limit =0 xfffff ,
+; 1st flags : ( present )1 ( privilege )00 ( descriptor type )1 -> 1001 b
+; type flags : ( code )1 ( conforming )0 ( readable )1 ( accessed )0 -> 1010 b
+; 2nd flags : ( granularity )1 (32 - bit default )1 (64 - bit seg )0 ( AVL )0 -> 1100 b
+dw 0xffff ; Limit ( bits 0 -15)
+dw 0x0 ; Base ( bits 0 -15)
+db 0x0 ; Base ( bits 16 -23)
+db 10011010b ; 1st flags , type flags
+db 11001111b ; 2nd flags , Limit ( bits 16 -19)
+db 0x0 ; Base ( bits 24 -31)
+gdt_data : ; the data segment descriptor
+; Same as code segment except for the type flags :
+; type flags : ( code )0 ( expand down )0 ( writable )1 ( accessed )0 -> 0010 b
+dw 0xffff ; Limit ( bits 0 -15)
+dw 0x0 ; Base ( bits 0 -15)
+db 0x0 ; Base ( bits 16 -23)
+db 10010010b ; 1st flags , type flags
+db 11001111b ; 2nd flags , Limit ( bits 16 -19)
+db 0x0 ; Base ( bits 24 -31)
+gdt_end : ; The reason for putting a label at the end of the
+; GDT is so we can have the assembler calculate
+; the size of the GDT for the GDT decriptor ( below )
+; GDT descriptior
+gdt_descriptor :
+dw gdt_end - gdt_start - 1 ; Size of our GDT , always less one
+; of the true size
+dd gdt_start ; Start address of our GDT
+; Define some handy constants for the GDT segment descriptor offsets , which
+; are what segment registers must contain when in protected mode. For example ,
+; when we set DS = 0 x10 in PM , the CPU knows that we mean it to use the
+; segment described at offset 0 x10 ( i.e. 16 bytes ) in our GDT , which in our
+; case is the DATA segment (0 x0 -> NULL ; 0x08 -> CODE ; 0 x10 -> DATA )
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+; --- THIS PART OF THE CODE IS COPIED FROM OS-DEV.pdf, I READ THROUGH THE EXPLANATION AND IT'S POINTLESS TO CRAFT THIS MYSELF AS OF NOW. ---
 
 times 510 - ($ - $$) db 0x0
 dw 0xaa55
