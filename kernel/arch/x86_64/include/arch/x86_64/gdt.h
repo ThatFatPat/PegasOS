86606f32deb24aded25b93d82e8be06d4390f460
ThatFatPat <idoshav@gmail.com>
2019-08-14 16:38:17 +0300

    Setup basic GDT

--- kernel/arch/x86_64/include/arch/x86_64/gdt.h
+++ kernel/arch/x86_64/include/arch/x86_64/gdt.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define GDT_ENTRY_SIZE 8
+
 #define GDT_FLAG_4K (1 << 3)
 #define GDT_FLAG_32 (1 << 2)
 #define GDT_FLAG_64 (1 << 1)
@@ -14,7 +16,7 @@
 
 // For details, see https://wiki.osdev.org/Global_Descriptor_Table.
 
-#define GDT_ENTRY(base, limit, flags, access)                    \
-    (((((base) >> 24) & 0xff) << 56) | (((flags)&0xf) << 52) |   \
-     ((((limit) >> 16) & 0xf) << 48) | (((access)&0xff) << 40) | \
-     ((((base) >> 16) & 0xfff) << 16) | ((limit)&0xffff))
+#define GDT_ENTRY(base, limit, flags, access)                   \
+  (((((base) >> 24) & 0xff) << 56) | (((flags) &0xf) << 52) |   \
+   ((((limit) >> 16) & 0xf) << 48) | (((access) &0xff) << 40) | \
+   ((((base) >> 16) & 0xfff) << 16) | ((limit) &0xffff))
