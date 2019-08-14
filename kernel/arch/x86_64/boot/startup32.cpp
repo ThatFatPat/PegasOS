7e10a119a5c181c13c21e30fd24b069e3f25ce73
ThatFatPat <idoshav@gmail.com>
2019-08-14 15:40:22 +0300

    IntelliSense Workaround
    Co-authored-by: Noam Raz <noamraz8@gmail.com>

--- kernel/arch/x86_64/boot/startup32.cpp
+++ kernel/arch/x86_64/boot/startup32.cpp
@@ -1,16 +1,16 @@
+#include <intellisense_workaround.h>
 #include <stdint.h>
 
-extern "C" int main()
-{
-    // Constants
-    uint32_t VIDEO_MEMORY = 0xb8000;
+extern "C" int main() {
+  // Constants
+  uint32_t VIDEO_MEMORY = 0xb8000;
 
-    // Pre-allocated
-    uint8_t pml4[4096];
-    uint8_t pdpte1[4096];
+  // Pre-allocated
+  uint8_t pml4[4096];
+  uint8_t pdpte1[4096];
 
-    // Code
-    uint32_t video_offset = 0x0;
-    char *hello = "Hello from\n\r";
-    return 0;
+  // Code
+  uint32_t video_offset = 0x0;
+  char* hello = "Hello from\n\r";
+  return 0;
 }
---
+++ kernel/include/intellisense_workaround.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// IntelliSense assumes we are hosted. To get accurate real-time IntelliSense,
+// we must specify that we are not hosted ourselves.
+#if __STDC_HOSTED__
+#undef __STDC_HOSTED__
+#endif
