5b9f3c727778b328760c9039b7ba7888eb5e571d
ThatFatPat <idoshav@gmail.com>
2019-08-14 14:31:39 +0300

    Succefully transition to CMake and GRUB. Add Multiboot2 compliance.
    Co-authored-by: Noam Raz <noamraz8@gmail.com>

--- connect_gdb.gdb
+++ connect_gdb.gdb
@@ -1,4 +1,4 @@
-target remote | qemu-system-i386 -gdb stdio boot.bin
+target remote | qemu-system-i386 -gdb stdio -cdrom ./build/PegasOS.iso
 # Next line for debugging in 16-bit
 # set tdesc filename target.xml
 set disassembly-flavor intel
---
+++ kernel/arch/x86_64/include/arch/x86_64/gdt.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define GDT_FLAG_4K (1 << 3)
+#define GDT_FLAG_32 (1 << 2)
+#define GDT_FLAG_64 (1 << 1)
+
+#define GDT_ACCESS_PRESENT (1 << 7)
+#define GDT_ACCESS_RING0 (0 << 5)
+#define GDT_ACCESS_RING3 (3 << 5)
+#define GDT_ACCESS_NON_SYSTEM (1 << 4)
+#define GDT_ACCESS_EXEC (1 << 3)
+#define GDT_ACCESS_DIR_DOWN (1 << 2)
+#define GDT_ACCESS_READ_WRITE (1 << 1)
+
+// For details, see https://wiki.osdev.org/Global_Descriptor_Table.
+
+#define GDT_ENTRY(base, limit, flags, access)                    \
+    (((((base) >> 24) & 0xff) << 56) | (((flags)&0xf) << 52) |   \
+     ((((limit) >> 16) & 0xf) << 48) | (((access)&0xff) << 40) | \
+     ((((base) >> 16) & 0xfff) << 16) | ((limit)&0xffff))
