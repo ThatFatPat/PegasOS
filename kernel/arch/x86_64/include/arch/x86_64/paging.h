81bb795951af02555dee012d3ae4e116ae6a1e5e
Noam Raz <noamraz8@gmail.com>
2019-09-05 07:42:17 +0200

    Add macros for computing page table indices

--- kernel/arch/x86_64/include/arch/x86_64/paging.h
+++ kernel/arch/x86_64/include/arch/x86_64/paging.h
@@ -9,18 +9,22 @@
 #define PML4_ALIGN 0x1000
 #define PML4_SIZE 0x1000
 #define PML4_ENTRY_SIZE 8
+#define PML4_IDX_FROM_ADDR(addr) (((addr) >> 39) & 0x1ff)
 
 #define PDPT_ALIGN 0x1000
 #define PDPT_SIZE 0x1000
 #define PDPT_ENTRY_SIZE 8
+#define PDPT_IDX_FROM_ADDR(addr) (((addr) >> 30) & 0x1ff)
 
 #define PAGE_DIRECTORY_ALIGN 0x1000
 #define PAGE_DIRECTORY_SIZE 0x1000
 #define PAGE_DIRECTORY_ENTRY_SIZE 8
+#define PAGE_DIRECTORY_IDX_FROM_ADDR(addr) (((addr) >> 21) & 0x1ff)
 
 #define PAGE_TABLE_ALIGN 0x1000
 #define PAGE_TABLE_SIZE 0x1000
 #define PAGE_TABLE_ENTRY_SIZE 8
+#define PAGE_TABLE_IDX_FROM_ADDR(addr) (((addr) >> 12) & 0x1ff)
 
 #define PAGE_FLAG_PRESENT (1 << 0)
 #define PAGE_FLAG_WRITE (1 << 1)
