b1ab666a064e756fba949d5364610b140f8d8112
Noam Raz <noamraz8@gmail.com>
2019-09-14 00:46:28 +0300

    Tweak mmu header

--- kernel/arch/x86_64/include/arch/x86_64/mmu.h
+++ kernel/arch/x86_64/include/arch/x86_64/mmu.h
@@ -1,15 +1,13 @@
 #pragma once
 
-#define X86_64_PAGE_SHIFT 12
-#define X86_64_PAGE_SIZE (1 << X86_64_PAGE_SHIFT) // 4KiB
+#define X86_64_PTE_MASK 0x1ff
+#define X86_64_PML4_SHIFT 39
+#define X86_64_PDPT_SHIFT 30
+#define X86_64_PDT_SHIFT 21
+#define X86_64_PT_SHIFT 12
 
-#define X86_64_PDT_PAGE_SHIFT 21
-#define X86_64_PDT_PAGE_SIZE (1 << X86_64_PDT_PAGE_SHIFT) // 2MiB
-
-#define X86_64_PML4_IDX_FROM_ADDR(addr) (((addr) >> 39) & 0x1ff)
-#define X86_64_PDPT_IDX_FROM_ADDR(addr) (((addr) >> 30) & 0x1ff)
-#define X86_64_PD_IDX_FROM_ADDR(addr) (((addr) >> 21) & 0x1ff)
-#define X86_64_PT_IDX_FROM_ADDR(addr) (((addr) >> 12) & 0x1ff)
+#define X86_64_PAGE_SIZE (1 << X86_64_PT_SHIFT)      // 4KiB
+#define X86_64_PDT_PAGE_SIZE (1 << X86_64_PDT_SHIFT) // 2MiB
 
 #define X86_64_PT_ALIGN 0x1000
 #define X86_64_PT_ENTRIES 0x200
