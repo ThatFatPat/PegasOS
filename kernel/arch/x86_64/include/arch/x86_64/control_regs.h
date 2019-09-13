83318e7f7ae53b5f75d3cfa2439d8d6077fc908f
Noam Raz <noamraz8@gmail.com>
2019-09-13 14:32:37 +0300

    Prefix all x86_64-specific macros

--- kernel/arch/x86_64/include/arch/x86_64/control_regs.h
+++ kernel/arch/x86_64/include/arch/x86_64/control_regs.h
@@ -2,16 +2,16 @@
 
 // Control registers
 
-#define CR0_PE (1 << 0)
-#define CR0_WP (1 << 16)
-#define CR0_PG (1 << 31)
+#define X86_64_CR0_PE (1 << 0)
+#define X86_64_CR0_WP (1 << 16)
+#define X86_64_CR0_PG (1 << 31)
 
-#define CR4_PAE (1 << 5)
+#define X86_64_CR4_PAE (1 << 5)
 
 // MSRs
 
-#define MSR_EFER 0xC0000080
-#define MSR_EFER_LME (1 << 8)
+#define X86_64_MSR_EFER 0xC0000080
+#define X86_64_MSR_EFER_LME (1 << 8)
 
 #ifndef __ASSEMBLER__
 
