8a5ef1a1720e0991a0e8e6ef6f11f4119dcc3ad8
Noam Raz <noamraz8@gmail.com>
2019-09-14 22:41:11 +0300

    Implement transition to c++

--- kernel/arch/x86_64/boot/early_entry.cpp
+++ kernel/arch/x86_64/boot/early_entry.cpp
@@ -1,1 +1,7 @@
 #include <arch/x86_64/control_regs.h>
+#include <stdint.h>
+
+extern "C" void x86_64_early_entry(uintptr_t multiboot_info_paddr) {
+  // Display L in light gray
+  *reinterpret_cast<uint16_t*>(0xb8000) = 0x700 | 'L';
+}
