990300859d1dc4ba7d3d2c3d5aa8f96fa09094ac
Noam Raz <noamraz8@gmail.com>
2019-09-17 00:11:34 +0300

    Invoke BSP initialization function in early entry

--- kernel/arch/x86_64/boot/early_entry.cpp
+++ kernel/arch/x86_64/boot/early_entry.cpp
@@ -1,11 +1,14 @@
 #include <arch/kernel_addrspace.h>
 #include <arch/x86_64/control_regs.h>
 #include <arch/x86_64/mmu.h>
+#include <arch/x86_64/mp.h>
 #include <stdint.h>
 
 extern "C" [[noreturn]] void x86_64_early_entry(
     uintptr_t multiboot_info_paddr) {
   arch::x86_64::mmu_init_phys_map();
+  arch::x86_64::init_bsp();
+
   // Display L in light gray
   *reinterpret_cast<uint16_t*>(ARCH_PHYS_MAP_BASE + 0xb8000) = 0x700 | 'L';
 
