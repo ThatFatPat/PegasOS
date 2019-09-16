60a663b84a021d7d6f7f1a307dd799805364e249
Noam Raz <noamraz8@gmail.com>
2019-09-16 22:17:34 +0300

    Remove always inline attributes

--- kernel/arch/x86_64/include/arch/x86_64/control_regs.h
+++ kernel/arch/x86_64/include/arch/x86_64/control_regs.h
@@ -19,49 +19,49 @@
 
 namespace arch::x86_64 {
 
-[[gnu::always_inline]] inline uint64_t read_cr0() {
+inline uint64_t read_cr0() {
   uint64_t ret;
   asm("mov %%cr0, %0" : "=r"(ret));
   return ret;
 }
 
-[[gnu::always_inline]] inline void write_cr0(uint64_t value) {
+inline void write_cr0(uint64_t value) {
   asm("mov %0, %%cr0" : : "r"(value));
 }
 
-[[gnu::always_inline]] inline uint64_t read_cr2() {
+inline uint64_t read_cr2() {
   uint64_t ret;
   asm("mov %%cr2, %0" : "=r"(ret));
   return ret;
 }
 
-[[gnu::always_inline]] inline uint64_t read_cr3() {
+inline uint64_t read_cr3() {
   uint64_t ret;
   asm("mov %%cr3, %0" : "=r"(ret));
   return ret;
 }
 
-[[gnu::always_inline]] inline void write_cr3(uint64_t value) {
+inline void write_cr3(uint64_t value) {
   asm("mov %0, %%cr3" : : "r"(value) : "memory");
 }
 
-[[gnu::always_inline]] inline uint64_t read_cr4() {
+inline uint64_t read_cr4() {
   uint64_t ret;
   asm("mov %%cr4, %0" : "=r"(ret));
   return ret;
 }
 
-[[gnu::always_inline]] inline void write_cr4(uint64_t value) {
+inline void write_cr4(uint64_t value) {
   asm("mov %0, %%cr4" : : "r"(value));
 }
 
-[[gnu::always_inline]] inline uint64_t read_msr(uint32_t id) {
+inline uint64_t read_msr(uint32_t id) {
   uint32_t low, high;
   asm("rdmsr" : "=d"(high), "=a"(low) : "c"(id));
   return static_cast<uint64_t>(high) << 32 | low;
 }
 
-[[gnu::always_inline]] inline void write_msr(uint32_t id, uint64_t value) {
+inline void write_msr(uint32_t id, uint64_t value) {
   uint32_t low = static_cast<uint32_t>(value);
   uint32_t high = static_cast<uint32_t>(value >> 32);
 
