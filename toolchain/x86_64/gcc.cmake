f9563be09fe09a14521478a6950000e0fdc44cbf
Noam Raz <noamraz8@gmail.com>
2019-09-17 22:57:01 +0300

    Move assembler flag into toolchain file

--- toolchain/x86_64/gcc.cmake
+++ toolchain/x86_64/gcc.cmake
@@ -2,3 +2,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 
 set(CMAKE_CXX_COMPILER ${GCC_TOOLCHAIN_BIN}/x86_64-elf-g++ CACHE PATH "CXX compiler")
 set(CMAKE_ASM_COMPILER ${GCC_TOOLCHAIN_BIN}/x86_64-elf-gcc CACHE PATH "ASM compiler")
+
+# Prevent gas from treating '/' as comment
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -Wa,--divide")
