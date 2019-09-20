0a58d46cd5cb4213314b1167c81f59970b83911d
Noam Raz <noamraz8@gmail.com>
2019-09-20 13:36:14 +0300

    Make page table names consistent

--- kernel/arch/x86_64/mmu.cpp
+++ kernel/arch/x86_64/mmu.cpp
@@ -10,10 +10,7 @@ alignas(X86_64_PT_ALIGN) volatile arch::x86_64::pt_entry_t
     high_pdpt[X86_64_PT_ENTRIES];
 
 alignas(X86_64_PT_ALIGN) volatile arch::x86_64::pt_entry_t
-    kernel_pdt[2 * X86_64_PT_ENTRIES];
-
-alignas(X86_64_PT_ALIGN) volatile arch::x86_64::pt_entry_t
-    phys_map_pdt[64 * X86_64_PT_ENTRIES];
+    phys_map_pd[64 * X86_64_PT_ENTRIES];
 
 namespace arch::x86_64 {
 namespace {
@@ -29,12 +26,12 @@ void mmu_init_phys_map() {
       X86_64_MMU_FLAG_PRESENT | X86_64_MMU_FLAG_WRITE | X86_64_MMU_FLAG_NX;
 
   high_pdpt[X86_64_PDPT_IDX_FROM_ADDR(ARCH_PHYS_MAP_BASE)] =
-      (reinterpret_cast<uintptr_t>(phys_map_pdt) - ARCH_KERNEL_VMA_OFFSET) |
+      (reinterpret_cast<uintptr_t>(phys_map_pd) - ARCH_KERNEL_VMA_OFFSET) |
       flags;
 
   for (size_t i = 0; i < 64 * X86_64_PT_ENTRIES; i++) {
     uintptr_t paddr = i * X86_64_PDT_PAGE_SIZE;
-    phys_map_pdt[i] = paddr | flags | X86_64_MMU_FLAG_LARGE;
+    phys_map_pd[i] = paddr | flags | X86_64_MMU_FLAG_LARGE;
   }
 
   pml4[0] = 0; // Remove temporary 1GiB identity map
