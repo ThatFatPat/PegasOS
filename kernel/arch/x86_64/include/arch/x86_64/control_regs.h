dd6351dd8142e3df6cbef80d6018b08fcc83b6fa
Noam Raz <noamraz8@gmail.com>
2019-09-25 22:12:29 +0300

    Mark control reg inline asm as volatile

--- kernel/arch/x86_64/include/arch/x86_64/control_regs.h
+++ kernel/arch/x86_64/include/arch/x86_64/control_regs.h
@@ -25,43 +25,43 @@ namespace arch::x86_64 {
 
 inline uint64_t read_cr0() {
   uint64_t ret;
-  asm("mov %%cr0, %0" : "=r"(ret));
+  asm volatile("mov %%cr0, %0" : "=r"(ret));
   return ret;
 }
 
 inline void write_cr0(uint64_t value) {
-  asm("mov %0, %%cr0" : : "r"(value));
+  asm volatile("mov %0, %%cr0" : : "r"(value));
 }
 
 inline uint64_t read_cr2() {
   uint64_t ret;
-  asm("mov %%cr2, %0" : "=r"(ret));
+  asm volatile("mov %%cr2, %0" : "=r"(ret));
   return ret;
 }
 
 inline uint64_t read_cr3() {
   uint64_t ret;
-  asm("mov %%cr3, %0" : "=r"(ret));
+  asm volatile("mov %%cr3, %0" : "=r"(ret));
   return ret;
 }
 
 inline void write_cr3(uint64_t value) {
-  asm("mov %0, %%cr3" : : "r"(value) : "memory");
+  asm volatile("mov %0, %%cr3" : : "r"(value) : "memory");
 }
 
 inline uint64_t read_cr4() {
   uint64_t ret;
-  asm("mov %%cr4, %0" : "=r"(ret));
+  asm volatile("mov %%cr4, %0" : "=r"(ret));
   return ret;
 }
 
 inline void write_cr4(uint64_t value) {
-  asm("mov %0, %%cr4" : : "r"(value));
+  asm volatile("mov %0, %%cr4" : : "r"(value));
 }
 
 inline uint64_t read_msr(uint32_t id) {
   uint32_t low, high;
-  asm("rdmsr" : "=d"(high), "=a"(low) : "c"(id));
+  asm volatile("rdmsr" : "=d"(high), "=a"(low) : "c"(id));
   return static_cast<uint64_t>(high) << 32 | low;
 }
 
@@ -69,7 +69,7 @@ inline void write_msr(uint32_t id, uint64_t value) {
   uint32_t low = static_cast<uint32_t>(value);
   uint32_t high = static_cast<uint32_t>(value >> 32);
 
-  asm("wrmsr" : : "c"(id), "d"(high), "a"(low));
+  asm volatile("wrmsr" : : "c"(id), "d"(high), "a"(low));
 }
 
 } // namespace arch::x86_64
