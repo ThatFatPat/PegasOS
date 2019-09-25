8b1fc2e234d930c56781e5167b5f122a7d74e5c3
Noam Raz <noamraz8@gmail.com>
2019-09-26 00:02:14 +0300

    Print welcome message

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,6 +1,8 @@
 #include <arch/arch.h>
+#include <arch/console.h>
 
 extern "C" [[noreturn]] void kernel_main() {
   arch::early_init();
+  arch::console_puts("Welcome to PegasOS!\n");
   arch::halt();
 }
