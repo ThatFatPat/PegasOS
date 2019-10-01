7e9843590de312a51f5865774b710315aa599119
Noam Raz <noamraz8@gmail.com>
2019-10-01 14:28:03 +0300

    Constrain byte shift operators

--- psl/include/psl/byte.h
+++ psl/include/psl/byte.h
@@ -1,18 +1,20 @@
 #pragma once
 
+#include <psl/type_traits.h>
+
 #include <stdint.h>
 
 namespace psl {
 
 enum class [[gnu::may_alias]] byte : uint8_t{};
 
-template <typename I>
+template <typename I, typename = enable_if_t<is_integral_v<I>>>
 constexpr byte operator<<(byte b, I shift) {
   return static_cast<byte>(
       static_cast<uint8_t>(static_cast<unsigned int>(b) << shift));
 }
 
-template <typename I>
+template <typename I, typename = enable_if_t<is_integral_v<I>>>
 constexpr byte operator>>(byte b, I shift) {
   return static_cast<byte>(
       static_cast<uint8_t>(static_cast<unsigned int>(b) >> shift));
@@ -38,12 +40,12 @@ constexpr byte operator~(byte b) {
 }
 
 
-template <typename I>
+template <typename I, typename = enable_if_t<is_integral_v<I>>>
 constexpr byte& operator>>=(byte& b, I shift) {
   return b = b >> shift;
 }
 
-template <typename I>
+template <typename I, typename = enable_if_t<is_integral_v<I>>>
 constexpr byte& operator<<=(byte& b, I shift) {
   return b = b << shift;
 }
