fd9d03bf01190c39a15e0bcf28bda6cd63a928b7
Noam Raz <noamraz8@gmail.com>
2019-10-04 11:12:10 +0300

    Implement additional <iterator> functions

--- psl/include/psl/iterator.h
+++ psl/include/psl/iterator.h
@@ -4,9 +4,47 @@
 
 namespace psl {
 
+template <typename C>
+constexpr auto size(const C& cont) -> decltype(cont.size()) {
+  return cont.size();
+}
+
 template <typename T, size_t N>
 constexpr size_t size(const T (&)[N]) {
   return N;
+}
+
+
+template <typename C>
+constexpr auto data(const C& cont) -> decltype(cont.data()) {
+  return cont.data();
+}
+
+template <typename T, size_t N>
+constexpr T* data(T (&arr)[N]) {
+  return arr;
+}
+
+
+template <typename C>
+constexpr auto begin(C& cont) -> decltype(cont.begin()) {
+  return cont.begin();
+}
+
+template <typename T, size_t N>
+constexpr T* begin(T (&arr)[N]) {
+  return arr;
+}
+
+
+template <typename C>
+constexpr auto end(C& cont) -> decltype(cont.end()) {
+  return cont.end();
+}
+
+template <typename T, size_t N>
+constexpr T* end(T (&arr)[N]) {
+  return arr + N;
 }
 
 } // namespace psl
