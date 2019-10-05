b96a0a5304ac5c653a5817e4c1881bd5deeed36e
Noam Raz <noamraz8@gmail.com>
2019-10-05 23:03:46 +0300

    Document cpuid header

--- kernel/arch/x86_64/include/arch/x86_64/cpuid.h
+++ kernel/arch/x86_64/include/arch/x86_64/cpuid.h
@@ -1,6 +1,25 @@
+/**
+ * @addtogroup x86_64
+ * @{
+ * @file cpuid.h CPU feature and identification helpers. */
+
 #pragma once
 
+/**
+ * Retrieve maximum supported extended `cpuid` function.
+ */
 #define X86_64_CPUID_FUNC_EXT_CPUID 0x80000000
+
+/**
+ * Retrieve extended processor info.
+ */
 #define X86_64_CPUID_FUNC_EXT_PROC_INFO 0x80000001
 
+
+/**
+ * Indicates CPU support for long mode when set in the `eax` register of `cpuid`
+ * page @ref X86_64_CPUID_FUNC_EXT_PROC_INFO.
+ */
 #define X86_64_CPU_FEATURE_LONG_MODE (1 << 29)
+
+/** @} */
