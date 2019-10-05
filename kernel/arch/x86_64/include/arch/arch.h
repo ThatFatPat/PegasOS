2e4a7c6c46fe30818dd1e7e073f8dead422417ed
Noam Raz <noamraz8@gmail.com>
2019-10-05 16:08:24 +0300

    Document main arch header

--- kernel/arch/x86_64/include/arch/arch.h
+++ kernel/arch/x86_64/include/arch/arch.h
@@ -1,9 +1,19 @@
+/** @file arch.h General architecture-specific header for x86_64 architecture.
+ */
+
 #pragma once
 
 namespace arch {
 
+/**
+ * Perform early architecture-specific initialization, including processing
+ * bootloader data and setting up the map of physical memory.
+ */
 void early_init();
 
+/**
+ * Halt the current processor.
+ */
 [[noreturn]] inline void halt() {
   asm("cli");
   while (true) {
