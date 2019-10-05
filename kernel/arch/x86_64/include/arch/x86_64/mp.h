a9bef4bb5c23b52e091596c5fecb40bda516b14f
Noam Raz <noamraz8@gmail.com>
2019-10-05 16:09:46 +0300

    Document x86_64 mp header

--- kernel/arch/x86_64/include/arch/x86_64/mp.h
+++ kernel/arch/x86_64/include/arch/x86_64/mp.h
@@ -1,7 +1,11 @@
+/** @file mp.h Multi-processor utilities. */
 #pragma once
 
 namespace arch::x86_64 {
 
+/**
+ * Initialize early control registers on the bootstrap processor.
+ */
 void init_bsp();
 
-}
+} // namespace arch::x86_64
