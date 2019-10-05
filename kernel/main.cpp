5749e5af3ef194b46bde6ef3b28b0a491127be0f
Noam Raz <noamraz8@gmail.com>
2019-10-05 16:19:30 +0300

    Create kernel module and documentation

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,8 +1,19 @@
+/**
+ * @addtogroup kernel
+ * @{
+ * @file main.cpp
+ */
+
 #include <arch/arch.h>
 #include <arch/console.h>
 
+/**
+ * Main kernel entry point.
+ */
 extern "C" [[noreturn]] void kernel_main() {
   arch::early_init();
   arch::console_puts("Welcome to PegasOS!\n");
   arch::halt();
 }
+
+/** @} */
