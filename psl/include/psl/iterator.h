c51ba21691133f15189c472ec67fdf64bf043b4d
Noam Raz <noamraz8@gmail.com>
2019-10-05 12:55:18 +0300

    Document psl iterator header

--- psl/include/psl/iterator.h
+++ psl/include/psl/iterator.h
@@ -1,47 +1,75 @@
+/** @file iterator.h Iterator utilites, inspired by <iterator>. */
+
 #pragma once
 
 #include <stddef.h>
 
 namespace psl {
 
+/**
+ * @return The size of the container `cont`.
+ */
 template <typename C>
 constexpr auto size(const C& cont) -> decltype(cont.size()) {
   return cont.size();
 }
 
+/**
+ * @return The size of the specified array.
+ */
 template <typename T, size_t N>
 constexpr size_t size(const T (&)[N]) {
   return N;
 }
 
 
+/**
+ * Retrieve a pointer to the data stored in the container `cont`, if it is a
+ * contiguous container.
+ * @return A pointer to the data stored in `cont`.
+ */
 template <typename C>
 constexpr auto data(const C& cont) -> decltype(cont.data()) {
   return cont.data();
 }
 
+/**
+ * Retrieve a pointer to the data stored in the array `arr`.
+ */
 template <typename T, size_t N>
 constexpr T* data(T (&arr)[N]) {
   return arr;
 }
 
 
+/**
+ * Retrieve an iterator pointing to the beginning of `cont`'s contents.
+ */
 template <typename C>
 constexpr auto begin(C& cont) -> decltype(cont.begin()) {
   return cont.begin();
 }
 
+/**
+ * Retrieve an iterator pointing to the beginning of array `arr`.
+ */
 template <typename T, size_t N>
 constexpr T* begin(T (&arr)[N]) {
   return arr;
 }
 
 
+/**
+ * Retrieve an iterator pointing past the end of `cont`'s contents.
+ */
 template <typename C>
 constexpr auto end(C& cont) -> decltype(cont.end()) {
   return cont.end();
 }
 
+/**
+ * Retrieve an iterator pointing past the end of array `arr`.
+ */
 template <typename T, size_t N>
 constexpr T* end(T (&arr)[N]) {
   return arr + N;
