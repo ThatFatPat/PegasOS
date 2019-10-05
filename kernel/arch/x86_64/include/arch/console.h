6c31496b4ebc6bae9007266fe51b5549956f4e36
Noam Raz <noamraz8@gmail.com>
2019-10-05 16:08:41 +0300

    Document console header

--- kernel/arch/x86_64/include/arch/console.h
+++ kernel/arch/x86_64/include/arch/console.h
@@ -4,10 +4,27 @@
 
 namespace arch {
 
+/**
+ * Initialize the console, including clearing the screen and setting up the
+ * cursor position.
+ */
 void console_init();
+
+/**
+ * Clear the console and reset the cursor.
+ */
 void console_clear();
 
+/**
+ * Write a character to the console.
+ * @param c The character to be written.
+ */
 void console_putc(char c);
+
+/**
+ * Write a string to the console.
+ * @param str The string to be written.
+ */
 void console_puts(psl::string_view str);
 
 } // namespace arch
