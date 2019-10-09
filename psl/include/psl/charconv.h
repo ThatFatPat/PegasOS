950c09d3a7d9644a3af4f407c2f5bbf0678c0f0b
Noam Raz <noamraz8@gmail.com>
2019-10-09 22:47:35 +0300

    Implement psl::to_chars
    Co-authored-by: ThatFatPat <idoshav@gmail.com>

--- psl/include/psl/charconv.h
+++ psl/include/psl/charconv.h
@@ -6,45 +6,82 @@
 
 #pragma once
 
+#include <limits.h>
+#include <psl/iterator.h>
 #include <psl/type_traits.h>
+#include <string.h>
 
 namespace psl {
+namespace impl {
 
 /**
- * Represents a status code returned by the character conversion functions.
+ * Lookup table used to convert digits to their character representation.
  */
-enum class charconv_status {
-  ok,
-  overflow,
-  invalid_arg,
-};
+constexpr char to_chars_digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+} // namespace impl
 
 /**
- * Result returned by to_chars.
- */
-struct to_chars_result {
-  char* ptr;              ///< Indicates the end of the formatted number.
-  charconv_status status; ///< Status code.
-};
-
-
-/**
- * Convert an integer value into a character string and store it in `[begin,
- * end)`.
- * @param begin Pointer to the start of the result buffer.
- * @param end Pointer past the end of the result buffer.
+ * Convert an integer value into a character string and store it in `[first,
+ * last)`, which must be a valid range.
+ * @param first Pointer to the start of the result buffer.
+ * @param last Pointer past the end of the result buffer.
  * @param value Value to convert.
  * @param base Numerical base to use. Must be between 2 and 36.
- * @return On success, a result with @ref psl::to_chars_result::status "status"
- * set to `ok` and @ref psl::to_chars_result::ptr "ptr"
- * set past the end of the formatted number. On error, a result with @ref
- * psl::to_chars_result::status "status" set to the appropriate error code and
- * @ref psl::to_chars_result::ptr "ptr" set to `end`.
- * @note This function only participates in overload resolution if `I` is an
- * integer type.
+ * @return On success, a pointer past the end of the converted integer. On error
+ * (if the buffer is too small), returns `nullptr`.
+ * @note This function does not null terminate the output buffer.
  */
 template <typename I, typename = enable_if_t<is_integral_v<I>>>
-to_chars_result to_chars(char* begin, char* end, I value, int base = 10) {}
+char* to_chars(char* first, char* last, I value, int base = 10) {
+  auto uvalue = static_cast<psl::make_unsigned_t<I>>(value);
+
+  if constexpr (is_signed_v<I>) {
+    if (value < 0) {
+      // No need to check here since [first, last) is required to be a valid
+      // range.
+      *first++ = '-';
+      uvalue = -value;
+    }
+  }
+
+  // Conservative size estimate, sufficient even for smallest base.
+  char buf[sizeof(I) * CHAR_BIT];
+  char* rnext = psl::end(buf);
+
+  switch (base) {
+    // Improved codegen for common cases
+  case 10:
+    do {
+      *--rnext = static_cast<char>(uvalue % 10 + '0');
+      uvalue /= 10;
+    } while (uvalue);
+    break;
+
+  case 16:
+    do {
+      *--rnext = impl::to_chars_digits[uvalue & 0xf];
+      uvalue >>= 4;
+    } while (uvalue);
+    break;
+
+    // Generic path
+  default:
+    do {
+      *--rnext = impl::to_chars_digits[uvalue % base];
+      uvalue /= base;
+    } while (uvalue);
+    break;
+  }
+
+  ptrdiff_t str_size = psl::end(buf) - rnext;
+  if (str_size > last - first) {
+    return nullptr;
+  }
+
+  memcpy(first, rnext, str_size);
+  return first + str_size;
+}
 
 } // namespace psl
 
