5266237cb556509079ec95ee9d202adbd6f4610f
Noam Raz <noamraz8@gmail.com>
2019-10-15 17:52:21 +0200

    Create psl::find

--- psl/include/psl/algorithm.h
+++ psl/include/psl/algorithm.h
@@ -6,6 +6,27 @@
 
 #pragma once
 
-namespace psl {}
+namespace psl {
+
+/**
+ * Search forward for `val` in `[first, last)`.
+ * @param first Input iterator indicating start of range.
+ * @param last Input iterator past end of range.
+ * @param val Value to search for.
+ * @return An iterator pointing to the first element in the range that is equal
+ * to `val`, or `last` if none is found.
+ */
+template <typename InIt, typename T>
+constexpr InIt find(InIt first, InIt last, const T& val) {
+  for (; first != last; ++first) {
+    if (*first == val) {
+      return first;
+    }
+  }
+
+  return last;
+}
+
+} // namespace psl
 
 /** @} */
