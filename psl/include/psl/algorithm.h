d1ac4c9d95984f229ba713c68825636f395f50b4
Noam Raz <noamraz8@gmail.com>
2019-10-19 23:48:44 +0300

    Create psl::find_if

--- psl/include/psl/algorithm.h
+++ psl/include/psl/algorithm.h
@@ -27,6 +27,28 @@ constexpr InIt find(InIt first, InIt last, const T& val) {
   return last;
 }
 
+/**
+ * Search forward for an element satisfying the given predicate in `[first,
+ * last)`.
+ * @param first Input iterator indicating start of range.
+ * @param last Input iterator past end of range.
+ * @param pred Predicate to use. The expression `pred(v)` must be convertible to
+ * `bool`, where `v` is a value of the iterator's value type.
+ * @return An iterator pointing to the first element in the range that satisfies
+ * `pred`, or `last` if none is found.
+ */
+template <typename InIt, typename P>
+constexpr InIt find_if(InIt first, InIt last, P pred) {
+  for (; first != last; ++first) {
+    if (pred(*first)) {
+      return first;
+    }
+  }
+
+  return last;
+}
+
+
 } // namespace psl
 
 /** @} */
