59250480b35693875f307a83c762e35e08b41771
Noam Raz <noamraz8@gmail.com>
2019-10-21 00:36:05 +0300

    Document formatting library

--- psl/include/psl/format.h
+++ psl/include/psl/format.h
@@ -1,7 +1,8 @@
 /**
  * @addtogroup psl
  * @{
- * @file format.h String formatting library.
+ * @file format.h String formatting library. For more details, see the @ref
+ * psl_fmt "formatting documentation".
  */
 
 #pragma once
@@ -14,6 +15,12 @@
 
 namespace psl {
 
+/**
+ * Class template that can be specialized to provide formatting capabilities for
+ * custom types. The second template argument defaults to void and can be used
+ * for SFINAE in partial specializations. For more details, see the @ref
+ * psl_fmt_formatter "formatter documentation".
+ */
 template <typename T, typename = void>
 struct formatter;
 
@@ -28,8 +35,8 @@ struct format_arg {
   template <typename T>
   constexpr format_arg(const T& val)
       : obj(::psl::addressof(val)),
-        output_func([](O& outputter, const void* obj, string_view spec) {
-          formatter<T>::format(outputter, *static_cast<T*>(obj), spec);
+        output_func([](O& output_sink, const void* obj, string_view spec) {
+          formatter<T>::format(output_sink, *static_cast<T*>(obj), spec);
         }) {}
 
   const void* obj;
@@ -55,9 +62,6 @@ public:
   constexpr format_args_ref(const format_arg_store<O, Args...>& args)
       : args_(args.args_), size_(sizeof...(Args)) {}
 
-  template <typename... Args>
-  format_args_ref(const format_arg_store<O, Args...>&&) = delete;
-
 private:
   template <typename T>
   friend void output_format_arg(T&, const format_args_ref<T>&, size_t&,
@@ -75,17 +79,17 @@ inline string_view extract_prefix(string_view& str, size_t count) {
 }
 
 template <typename O>
-bool handle_escaped(O& outputter, string_view& str, char c) {
+bool handle_escaped(O& output_sink, string_view& str, char c) {
   if (!str.empty() && str.front() == c) {
     str.remove_prefix(1);
-    outputter(string_view{&c, 1});
+    output_sink(string_view{&c, 1});
     return true;
   }
   return false;
 }
 
 template <typename O>
-void output_format_arg(O& outputter, const format_args_ref<O>& args,
+void output_format_arg(O& output_sink, const format_args_ref<O>& args,
                        size_t& curr_arg, string_view spec) {
   if (curr_arg >= args.size_) {
     return;
@@ -97,15 +101,29 @@ void output_format_arg(O& outputter, const format_args_ref<O>& args,
                               : spec.substr(user_delim_pos - spec.begin() + 1);
 
   const auto& arg = args.args_[curr_arg++];
-  arg.output_func(outputter, arg.obj, user_spec);
+  arg.output_func(output_sink, arg.obj, user_spec);
 }
 
 } // namespace impl
 
 
+/**
+ * Opaque type which can be used to reference type-erased formatting arguments
+ * created by @ref psl::make_format_args.
+ * @note To prevent dangling references, objects of this type should not outlive
+ * the formatting argument objects (created by @ref psl::make_format_args) to
+ * which they refer.
+ */
 template <typename O>
 using format_args_ref_t = impl::format_args_ref<type_identity_t<O>>;
 
+/**
+ * Create a type-erased object storing references to formatting arguments in a
+ * format suitable for output through @ref psl_fmt_output_sink "output sink"
+ * `O`.
+ * @note The exact return type of this function is unspecified, but is
+ * guaranteed to be convertible to @ref psl::format_args_ref_t.
+ */
 template <typename O, typename... Args>
 constexpr impl::format_arg_store<O, Args...> make_format_args(
     const Args&... args) {
@@ -113,15 +131,26 @@ constexpr impl::format_arg_store<O, Args...> make_format_args(
 }
 
 
+/**
+ * Format the @ref psl_fmt_str "format string" `fmt` with the type-erased
+ * arguments stored in `args`, reporting output to `output_sink`.
+ * @param output_sink The @ref psl_fmt_output_sink "output sink" to use.
+ * @param fmt The @ref psl_fmt_str "format string" to use.
+ * @param args A reference to the object holding type-erased formatting
+ * arguments. Use @ref psl::make_format_args to create such an object.
+ * @note If the format string is invalid, or if there are more @ref
+ * psl_fmt_str_subst "substitutions" in the format string than formatting
+ * arguments, the output of this function is unspecified.
+ */
 template <typename O>
-void vformat(O& outputter, string_view fmt, format_args_ref_t<O> args) {
+void vformat(O& output_sink, string_view fmt, format_args_ref_t<O> args) {
   size_t curr_arg = 0;
 
   while (!fmt.empty()) {
     auto brace_pos = find_if(fmt.begin(), fmt.end(),
                              [](char c) { return c == '{' || c == '}'; });
     auto brace_off = brace_pos - fmt.begin();
-    outputter(impl::extract_prefix(fmt, brace_off));
+    output_sink(impl::extract_prefix(fmt, brace_off));
 
     if (fmt.empty()) {
       // Nothing more to do
@@ -132,7 +161,7 @@ void vformat(O& outputter, string_view fmt, format_args_ref_t<O> args) {
 
     switch (*brace_pos) {
     case '{':
-      if (impl::handle_escaped(outputter, fmt, '{')) {
+      if (impl::handle_escaped(output_sink, fmt, '{')) {
         break;
       }
 
@@ -145,13 +174,13 @@ void vformat(O& outputter, string_view fmt, format_args_ref_t<O> args) {
 
         auto spec = impl::extract_prefix(fmt, close_brace_pos - fmt.begin());
         fmt.remove_prefix(1); // Skip '}'
-        impl::output_format_arg(outputter, args, curr_arg, spec);
+        impl::output_format_arg(output_sink, args, curr_arg, spec);
       }
 
       break;
 
     case '}':
-      if (!impl::handle_escaped(outputter, fmt, '}')) {
+      if (!impl::handle_escaped(output_sink, fmt, '}')) {
         // Invalid format string
         return;
       }
@@ -160,10 +189,19 @@ void vformat(O& outputter, string_view fmt, format_args_ref_t<O> args) {
   }
 }
 
+/**
+ * Format the @ref psl_fmt_str "format string" `fmt` with the arguments `args`,
+ * reporting output to `output_sink`.
+ * @param output_sink The @ref psl_fmt_output_sink "output sink" to use.
+ * @param fmt The @ref psl_fmt_str "format string" to use.
+ * @param args The formatting arguments to use.
+ * @note If the format string is invalid, or if there are more @ref
+ * psl_fmt_str_subst "substitutions" in the format string than formatting
+ * arguments, the output of this function is unspecified.
+ */
 template <typename O, typename... Args>
-void format(O& outputter, string_view fmt, const Args&... args) {
-  auto erased_args = make_format_args<O>(args...);
-  vformat(outputter, fmt, erased_args);
+void format(O& output_sink, string_view fmt, const Args&... args) {
+  vformat(output_sink, fmt, make_format_args<O>(args...));
 }
 
 } // namespace psl
